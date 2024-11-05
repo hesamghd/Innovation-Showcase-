edee74a79627ed4c571b5134b3e6147ff1924fd5
Hesam Ghanbarzadeh <49495017+hesamghd@users.noreply.github.com>
2024-11-05 19:54:36 +0100

    Image Binarization

--- Image Processing/Matlab/Chapter 2 - Image Thresholding/Simple Mean Binarization/PadArray.m
+++ Image Processing/Matlab/Chapter 2 - Image Thresholding/Simple Mean Binarization/PadArray.m
@@ -5,45 +5,37 @@ function PadIm = PadArray(I, Size, Dir, Type)
 % `Dir` & `Type`, Padding Is Applied Accordingly for Each Case to Handle 
 % Boundaries Effectively & Create a Consistent Padded Result.
 
-%% Step 1: Set Default Values for Dir and Type
-if nargin < 3 || isempty(Dir)
-    Dir = 'Both';                                                            % Default Direction is 'both'
-end
-if nargin < 4 || isempty(Type)
-    Type = 'zeros';                                                          % Default Padding Type is 'zeros'
-end
-
-%% Step 2: Check if the Input is Grayscale or Color
+%% Step 1: Check if the Input is Grayscale or Color
 if ndims(I) == 3
-    % Step 2-1: Separate the Color Channels & Pad Each Channel Individually
+    % Step 1-1: Separate the Color Channels & Pad Each Channel Individually
     R = PadArray(I(:, :, 1), Size, Dir, Type);
     G = PadArray(I(:, :, 2), Size, Dir, Type);
     B = PadArray(I(:, :, 3), Size, Dir, Type);
-    % Step 2-2: Concatenate Padded Channels to form a Color Image
+    % Step 1-2: Concatenate Padded Channels to form a Color Image
     PadIm = cat(3, R, G, B);
     return;
 end
 
-%% Step 3: Initialize Variables
+%% Step 2: Initialize Variables
 [r, c] = size(I);
 Pr = Size(1); Pc = Size(2);
 
 if strcmp(Dir, 'Both')
-    % Step 3-1: Pad on both sides
+    % Step 2-1: Pad on both sides
     PadIm = zeros(r + 2 * Pr, c + 2 * Pc, 'like', I);
     Rs = Pr + 1;
     Re = Rs + r - 1;
     Cs = Pc + 1;
     Ce = Cs + c - 1;
 elseif strcmp(Dir, 'Pre')
-    % Step 3-2: Pad only before rows and columns
+    % Step 2-2: Pad only before rows and columns
     PadIm = zeros(r + Pr, c + Pc, 'like', I);
     Rs = Pr + 1;
     Re = Rs + r - 1;
     Cs = Pc + 1;
     Ce = Cs + c - 1;
 elseif strcmp(Dir, 'Post')
-    % Step 3-3: Pad only after rows and columns
+    % Step 2-3: Pad only after rows and columns
     PadIm = zeros(r + Pr, c + Pc, 'like', I);
     Rs = 1;
     Re = Rs + r - 1;
@@ -52,23 +44,25 @@ elseif strcmp(Dir, 'Post')
 end
 PadIm(Rs:Re, Cs:Ce) = I;
 
-%% Step 4: Apply Padding Based on Type
+%% Step 3: Apply Padding Based on Type
 switch Type
-    % Step 4-1: Zero Padding is Already set by Default During Initialization
+    % Step 3-1: Zero Padding 
     case 'zeros'
-
+    % Step 3-2: One Padding 
     case 'ones'
         PadIm(:) = 1;                                                       % Fill the Entire Padded Image with Ones
         PadIm(Rs:Re, Cs:Ce) = I;                                            % Place the Original Image in the Center of the Padded Image
 
     case 'Replicate'
         if strcmp(Dir, 'Both') || strcmp(Dir, 'Pre')
+            % Step 3-3: Pre & Both-Padding for Replicate
             PadIm(1:Rs - 1, Cs:Ce) = repmat(I(1, :), Pr, 1);                % Top Padding: Replicate the First Row of the Image
             PadIm(Rs:Re, 1:Cs - 1) = repmat(I(:, 1), 1, Pc);                % Left Padding: Replicate the First Column of the Image
             PadIm(1:Rs - 1, 1:Cs - 1) = I(1, 1);                            % Top-Left Corner
             PadIm(1:Rs - 1, Ce + 1:end) = I(1, end);                        % Top-Right Corner
         end
         if strcmp(Dir, 'Both') || strcmp(Dir, 'Post')
+            % Step 3-3: Post & Both-Padding for Replicate
             PadIm(Re + 1:end, Cs:Ce) = repmat(I(end, :), Pr, 1);            % Bottom Padding: Replicate the Last Row of the Image
             PadIm(Rs:Re, Ce + 1:end) = repmat(I(:, end), 1, Pc);            % Right Padding
             PadIm(Re + 1:end, 1:Cs - 1) = I(end, 1);                        % Bottom-Left Corner
@@ -77,17 +71,17 @@ switch Type
 
     case 'Circular'
         if strcmp(Dir, 'Pre')
-            % Step 4-2: Pre-padding for circular
+            % Step 3-4: Pre-Padding for Circular
             PadIm(1:Rs - 1, Cs:Ce) = I(end - Pr + 1:end, :);                % Top Padding
             PadIm(Rs:Re, 1:Cs - 1) = I(:, end - Pc + 1:end);                % Left Padding
             PadIm(1:Rs - 1, 1:Cs - 1) = I(end - Pr + 1:end, end - Pc + 1:end); % Top-Left Corner
         elseif strcmp(Dir, 'Post')
-            % Step 4-3: Post-padding for circular
+            % Step 3-5: Post-Padding for Circular
             PadIm(Re + 1:end, Cs:Ce) = I(1:Pr, :);                          % Bottom Padding
             PadIm(Rs:Re, Ce + 1:end) = I(:, 1:Pc);                          % Right Padding
             PadIm(Re + 1:end, Ce + 1:end) = I(1:Pr, 1:Pc);                  % Bottom-Right Corner
         elseif strcmp(Dir, 'Both')
-            % Step 4-4: Both-sides padding for circular
+            % Step 4-6: Both-sides Padding for Circular
             PadIm(1:Rs - 1, Cs:Ce) = I(end - Pr + 1:end, :);                % Top Padding
             PadIm(Re + 1:end, Cs:Ce) = I(1:Pr, :);                          % Bottom Padding
             PadIm(Rs:Re, 1:Cs - 1) = I(:, end - Pc + 1:end);                % Left Padding
@@ -100,17 +94,17 @@ switch Type
 
     case 'Symmetric'
         if strcmp(Dir, 'Pre')
-            % Step 4-5: Pre-padding for symmetric
+            % Step 3-7: Pre-Padding for Symmetric
             PadIm(1:Rs - 1, Cs:Ce) = flipud(I(1:Pr, :));                    % Top Padding
             PadIm(Rs:Re, 1:Cs - 1) = fliplr(I(:, 1:Pc));                    % Left Padding
             PadIm(1:Rs - 1, 1:Cs - 1) = flipud(fliplr(I(1:Pr, 1:Pc)));      % Top-Left Corner
         elseif strcmp(Dir, 'Post')
-            % Step 4-6: Post-padding for symmetric
+            % Step 3-8: Post-Padding for Symmetric
             PadIm(Re + 1:end, Cs:Ce) = flipud(I(end - Pr + 1:end, :));      % Bottom Padding
             PadIm(Rs:Re, Ce + 1:end) = fliplr(I(:, end - Pc + 1:end));      % Right Padding
             PadIm(Re + 1:end, Ce + 1:end) = flipud(fliplr(I(end - Pr + 1:end, end - Pc + 1:end))); % Bottom-Right Corner
         elseif strcmp(Dir, 'Both')
-            % Step 4-7: Both-sides padding for symmetric
+            % Step 3-9: Both-sides Padding for Symmetric
             PadIm(1:Rs - 1, Cs:Ce) = flipud(I(1:Pr, :));                    % Top Padding
             PadIm(Re + 1:end, Cs:Ce) = flipud(I(end - Pr + 1:end, :));      % Bottom Padding
             PadIm(Rs:Re, 1:Cs - 1) = fliplr(I(:, 1:Pc));                    % Left Padding
